b8e5e5f73b961c1ffff3aef10e519e27fa99cede
PolPtoAmo <pvallverdualcaide@gmail.com>
2021-10-23 00:52:29 +0200

    Loading .env and loging into the bot

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log(process.env.TEXT)
+import { Intents } from "discord.js/typings/index.js";
+import { DisBot } from "disnine.js";
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
+
+const bot = new DisBot(
+  { clientOptions: { intents: [Intents.FLAGS.GUILDS] } },
+  { commandsPath: path.join(__dirname, "commands/") },
+  { listenersPath: path.join(__dirname, "listeners/") }
+);
+
+bot.login(process.env.TOKEN === undefined ? "" : process.env.TOKEN);
